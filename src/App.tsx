f084af6941ac551c915dc60468727a51eff3827d
dellgreen <fousenh2@gmail.com>
2025-03-10 16:20:36 +0900

    ナビゲーションの追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,28 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Profile from './components/Profile/Profile';
 import Monologue from './components/Monologue/Monologue';
 import MonologueForm from './components/Monologue/MonologueForm';
 
 function App() {
   return (
-    <div className="App">
-      <main className="main-content">
-        <Profile />
-        <Monologue post="つぶやきだお" post2="だおだお" />
-        <MonologueForm />
-      </main>
-    </div>
+    <Router>
+      <div className="App">
+        <nav>
+          <Link to="/">ホーム</Link>
+          <Link to="/todoPosts">TODOPosts</Link>
+        </nav>
+        <Routes>
+          <Route path="/todoPosts" element={<MonologueForm />} />
+        </Routes>
+        <main className="main-content">
+          <Profile />
+          <Monologue post="つぶやきだお" post2="だおだお" />
+          <MonologueForm />
+        </main>
+      </div>
+    </Router>
   );
 }
 
